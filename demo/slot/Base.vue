48051db8eca7cb4505bc8ae6b50bc5770280051f
Glodie Tshimini <glodie.tshimini@gmail.com>
2023-06-20 14:56:50 +0200

    feat: demo slot

---
+++ demo/slot/Base.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Base dynamique grâce au slot</h1>
+    <div>
+        <slot name="list"></slot>
+    </div>
+    <div>
+        <slot name="images">
+            
+        </slot>
+    </div>
+    <div>
+        <slot name="table"></slot>
+    </div>
+</template>
---
+++ demo/slot/Parent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Base>
+      <template #list>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </template>
+      <template #images>
+        <img
+          src="@/assets/img/img.png"
+          alt=""
+        />
+      </template>
+      <template #table>
+        <table>
+          <thead>
+            <tr>
+              <th>Colonne 1</th>
+              <th>Colonne 2</th>
+            </tr>
+          </thead>
+        </table>
+      </template>
+    </Base>
+  </div>
+</template>
+<script>
+import Base from "./Base.vue";
+export default {
+  components: {
+    Base,
+  },
+};
+</script>
