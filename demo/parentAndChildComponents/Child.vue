fb35ebce7be641f22c986e18bb93fed1001575b3
Glodie Tshimini <glodie.tshimini@gmail.com>
2023-06-20 11:37:40 +0200

    feat: components change data

---
+++ demo/parentAndChildComponents/Child.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }, 
+        content: {
+            type: String,
+            default: 'the content'
+        },
+        year: {
+            required: true,
+            validator(data) {
+                return /^(19|20)[\d]{2,2}$/.test(data)
+            }
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+    <p>{{ year }}</p>
+</template>
---
+++ demo/parentAndChildComponents/Parent.vue
@@ -0,0 +1,40 @@
+<script>
+import Child from './Child.vue';
+
+export default {
+    components: Child,
+    data() {
+        return {
+            items : [
+                {
+                    id: 1,
+                    title: 'title 1',
+                    content: 'content 1',
+                    'year': 2014
+                },
+                {
+                    id: 2,
+                    title: 'title 2',
+                    content: 'content 2',
+                    'year': 2019
+                },
+                {
+                    id: 3,
+                    title: 'title 3',
+                    content: 'content 3',
+                    'year': 2021
+                }
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <child 
+        v-for="item in items"
+        :key="item.id"
+        :title="item.id"
+        :content="item"
+        :year="item.year"
+    />
+</template>
